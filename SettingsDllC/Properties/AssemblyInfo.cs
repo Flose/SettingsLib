369849d1070b698fbd8678aa5aeb267bcc95722e
Sebastian <asamk@gmx.de>
2011-03-18 18:26:14 +0100

    AssemblyInfo.cs ausgefüllt

--- SettingsDllC/Properties/AssemblyInfo.cs
+++ SettingsDllC/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("SettingsDllC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SettingsDll")]
+[assembly: AssemblyDescription("A Class Library for storing Settings in a file.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SettingsDllC")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCompany("Flo & Seb Engineering")]
+[assembly: AssemblyProduct("SettingsDll")]
+[assembly: AssemblyCopyright("Copyright © Flo & Seb Engineering 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
