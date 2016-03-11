446445dc328aa65e7d6f229c9b1e07d48013b1d0
Sebastian <asamk@gmx.de>
2016-03-11 17:30:04 +0100

    Use FloseCode.SettingsLib as nuget id

--- SettingsLib/make_package.bat
+++ SettingsLib/make_package.bat
@@ -2,8 +2,8 @@ REM Build Solution
 set PATH_SOURCE_PROJ="%cd%\SettingsLib.csproj"
 
 FOR %%C IN (Release20 Release40) DO (
- "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_PROJ% /p:Configuration=%%C
+ "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_PROJ% /t:Rebuild /p:Configuration=%%C
  if errorlevel 1 goto :eof
 )
 
-nuget pack %PATH_SOURCE_PROJ% -Prop Configuration=Release20 -IncludeReferencedProjects -Verbosity detailed
+nuget pack %PATH_SOURCE_PROJ% -Prop Configuration=Release20 -Symbols -IncludeReferencedProjects -Verbosity detailed
